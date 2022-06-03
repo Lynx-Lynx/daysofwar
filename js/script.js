39a4f6e171927ff308a2e7dc8ad76d6809642470
Elena <delinne.weird@gmail.com>
2022-06-04 00:26:29 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 const LANG = document.documentElement.lang;
 const DAY = document.getElementById('main_text-days');
 const DATE = document.getElementsByClassName('main_date')[0];
-const START = '2022/02/23';
-const TODAY = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-const DIFFMS   = new Date(TODAY) - new Date(START);
+//Short dates are written with an "MM/DD/YYYY" syntax
+const START = '02/23/2022'; //-1 day to include 24th
+let TODAY = new Date().toLocaleString('en-US', { timeZone: 'Europe/Kiev' });
+TODAY = TODAY.slice(0,TODAY.indexOf(','));
 
+const DIFFMS = new Date(TODAY) - new Date(START);
+
+DATE.textContent = LANG === 'en' 
+  ? new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(new Date(TODAY)) 
+  : new Intl.DateTimeFormat('uk-UA', { dateStyle: 'full' }).format(new Date(TODAY));
 DAY.textContent = Math.round(DIFFMS / (1000 * 60 * 60 * 24));
-
-(function showDate() {
-  const now = new Date();
-  const dd = now.getDate();
-  const monthsEN = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-  const monthsUA = ["січня","лютого","березня","квітня","травня","червня","липня","серпня","вересня","жовтня","листопада","грудня"];
-  const mm = LANG === 'en' ? monthsEN[now.getMonth()] : monthsUA[now.getMonth()];
-  const yyyy = now.getFullYear();
-  DATE.textContent = `${dd < 10 ? '0' + dd : dd} ${mm} ${yyyy}`;
-})()
